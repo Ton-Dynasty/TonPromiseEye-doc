5c39e01d266899723091d4bf414ca43adc2d9674
蔡育呈 <55632018+WaterSo0910@users.noreply.github.com>
2023-09-14 05:38:50 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'eventon',
-  tagline: 'A flexible message hub for event on TON.',
+  tagline: 'A flexible message hub for event on TON',
   favicon: 'https://github.com/Ton-Dynasty/eventon-doc/blob/main/static/img/eventon.png?raw=true',
 
   // Set the production url of your site here
