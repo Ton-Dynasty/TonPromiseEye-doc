90a09949645818f25ab8f832a6d41c4b21993e6c
skyline9981 <skyline.cs12@nycu.edu.tw>
2023-09-13 12:12:23 +0800

    Update home page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,31 +1,55 @@
+// HomepageFeatures.tsx
+
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Blacklist',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        <div className={styles.textBlock}>
+            One such use case of this innovative system is the Blacklist functionality. This document delves deep into how professional entities can leverage Eventon to dispatch signals from off-chain, signifying updates to a blacklist.
+        </div>
+      </>
+    ),
+  },
+  {
+    title: 'Copy Trading',
+    description: (
+      <>
+        <div className={styles.textBlock}>
+          Another use case of this innovative system is the Whitelist functionality.This document delves deep into how professional entities can leverage Eventon to dispatch signals from off-chain, signifying updates to a whitelist.
+        </div>
+        
+      </>
+    ),
+  },
+  {
+    title: 'Vulnerable Contract Alert',
+    description: (
+      <>
+        <div className={styles.textBlock}>
+        The VulnerableContractAlert system is designed to detect and alert users to potential vulnerabilities in smart contracts within blockchain networks.
+        <h1></h1>
+        Smart contracts are critical components of blockchain ecosystems, and identifying vulnerabilities is essential to prevent security breaches and financial losses.
+        </div>
       </>
     ),
   },
 ];
 
-function Feature({ title, Svg, description }: FeatureItem) {
+function Feature({ title, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* <img src={ImgSrc} alt={title} className={styles.featureImg} /> */}
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
@@ -38,11 +62,12 @@ function Feature({ title, Svg, description }: FeatureItem) {
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
-      <div className="container">
+      <div className="container text--center"> {/* 使用 text--center 类 */}
+        <h1>Use Cases Example</h1> 
         <div className="row">
-          {/* {FeatureList.map((props, idx) => (
+          {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
-          ))} */}
+          ))}
         </div>
       </div>
     </section>
